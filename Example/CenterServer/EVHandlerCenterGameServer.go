8b57ca7ce1635167c235c25331f9327b381fd320
zhk <zhk@zhksoft.com>
2021-05-03 21:24:37 +0800

    pointer

--- Example/CenterServer/EVHandlerCenterGameServer.go
+++ Example/CenterServer/EVHandlerCenterGameServer.go
@@ -12,7 +12,7 @@ type EVHandlerCenterGameServer struct {
 	Network.EventHandler
 }
 
-func (ev EVHandlerCenterGameServer) OnOpened() (opts Network.Options, action Network.Action) {
+func (ev *EVHandlerCenterGameServer) OnOpened() (opts Network.Options, action Network.Action) {
 	slog.Info("EVHandlerCenterGameServer.OnOpened")
 
 	opts = Network.Options{TCPKeepAlive: time.Minute, ReuseInputBuffer: true}
@@ -20,20 +20,20 @@ func (ev EVHandlerCenterGameServer) OnOpened() (opts Network.Options, action Net
 	return
 }
 
-func (ev EVHandlerCenterGameServer) OnRecvMsg(b []byte) Network.Action {
+func (ev *EVHandlerCenterGameServer) OnRecvMsg(b []byte) Network.Action {
 	slog.Info("EVHandlerCenterGameServer.OnRecvMsg")
 
 	return Network.None
 }
 
-func (ev EVHandlerCenterGameServer) OnClosed(err error) (action Network.Action) {
+func (ev *EVHandlerCenterGameServer) OnClosed(err error) (action Network.Action) {
 	slog.Info("EVHandlerCenterGameServer.OnClosed")
 
 	action = Network.None
 	return
 }
 
-func (ev EVHandlerCenterGameServer) OnDetached(rwc io.ReadWriteCloser) (action Network.Action) {
+func (ev *EVHandlerCenterGameServer) OnDetached(rwc io.ReadWriteCloser) (action Network.Action) {
 	slog.Info("EVHandlerCenterGameServer.OnDetached")
 
 	rwc.Close()
--- Example/CenterServer/EVHandlerGMServer.go
+++ Example/CenterServer/EVHandlerGMServer.go
@@ -12,7 +12,7 @@ type EVHandlerGMServer struct {
 	Network.EventHandler
 }
 
-func (ev EVHandlerGMServer) OnOpened() (opts Network.Options, action Network.Action) {
+func (ev *EVHandlerGMServer) OnOpened() (opts Network.Options, action Network.Action) {
 	slog.Info("EVHandlerGMServer.OnOpened")
 
 	opts = Network.Options{TCPKeepAlive: time.Minute, ReuseInputBuffer: true}
@@ -20,20 +20,20 @@ func (ev EVHandlerGMServer) OnOpened() (opts Network.Options, action Network.Act
 	return
 }
 
-func (ev EVHandlerGMServer) OnRecvMsg(b []byte) Network.Action {
+func (ev *EVHandlerGMServer) OnRecvMsg(b []byte) Network.Action {
 	slog.Info("EVHandlerGMServer.OnRecvMsg")
 
 	return Network.None
 }
 
-func (ev EVHandlerGMServer) OnClosed(err error) (action Network.Action) {
+func (ev *EVHandlerGMServer) OnClosed(err error) (action Network.Action) {
 	slog.Info("EVHandlerGMServer.OnClosed")
 
 	action = Network.None
 	return
 }
 
-func (ev EVHandlerGMServer) OnDetached(rwc io.ReadWriteCloser) (action Network.Action) {
+func (ev *EVHandlerGMServer) OnDetached(rwc io.ReadWriteCloser) (action Network.Action) {
 	slog.Info("EVHandlerGMServer.OnDetached")
 
 	rwc.Close()
